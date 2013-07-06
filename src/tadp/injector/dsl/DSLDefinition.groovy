ccab365f601b1c87d0d1d1a81c9b47128cac031a
Jose Flores <jose.flores@mercadolibre.com>
2013-07-05 22:49:20 -0300

    Working

--- src/tadp/injector/dsl/DSLDefinition.groovy
+++ src/tadp/injector/dsl/DSLDefinition.groovy
@@ -5,19 +5,16 @@ import tadp.dependency.injector.fixture.velador.Alumbrable
 import tadp.dependency.injector.fixture.velador.Enchufable
 import tadp.dependency.injector.fixture.velador.EnchufeTresPatas
 import tadp.dependency.injector.fixture.velador.LamparitasOsram
+import tadp.dependency.injector.fixture.velador.Mampara;
+import tadp.dependency.injector.fixture.velador.MamparaConDibujitos;
+import tadp.dependency.injector.fixture.velador.MamparaSobria;
 import tadp.dependency.injector.fixture.velador.Velador
 import tadp.dependency.injector.structure.InjectionStructure
-
+import tadp.dependency.injector.structure.SetterInjectionStructure
+import tadp.dependency.injector.fixture.velador.SetterVeladorConOsramStructure
 
 class DSLDefinition {
 		
-	static public def un = "nada"
-	static public def con = "nada"
-	
-	static{
-		InjectionEngine.metaClass {
-		}
-	}
 	
 	static def motorDeInyeccion(bloque) { 
 		def ib = new InjectionBuilder()
@@ -33,28 +30,68 @@ class InjectionBuilder{
 	def aConstruir
 			
 	def usando( quienClass ) {
-		def relation = new RelationBuilder(esQue: quienClass)
-		relations.add(relation)
+		def relation = new RelationBuilder(quien: quienClass)
+		relations.push(relation)
 		return relation
 	}
+
 	
 	def dame(claseDelObjeto) {
 		aConstruir = claseDelObjeto
 	}
 	
+	
+	
 	def build(){
-		
-		InjectionStructure structure = new InjectionStructure(){
+		/*InjectionStructure structure = new InjectionStructure(){
 			void defineParts() {
 				relations.each {	
 					define(it.quien, it.esQue);
 				}
 			}
-		}
-		
-		def motor = new InjectionEngine(structure)
+		}*/
+		//def InjectionStructure structure = new SetterVeladorConOsramStructure()
+		def motor = InjectionEngine.createEngine(buildStructureFrom(relations));
 		motor.sparkOfLife(aConstruir)
 	}
+	
+	def buildStructureFrom(relations){
+		def InjectionStructure structure = new GenericStructure(relations)
+		structure.imprimiteAlgo()
+		structure
+	}
+	
+}
+
+class GenericStructure extends SetterInjectionStructure
+{
+	def relations
+	GenericStructure(relaciones){
+		relations = relaciones
+	}
+	
+	def imprimiteAlgo(){println relations.size()}
+	
+	@Override
+	void defineParts() {
+		relations.each {
+					define(it.quien, it.esQue);
+				}
+	}
+	/*@Override
+	public void defineParts() {
+		this.define(Velador.class, Velador.class);
+		this.define(Alumbrable.class, LamparitasOsram.class);
+		this.define(Mampara.class, MamparaSobria.class);
+		this.define(MamparaConDibujitos.class, MamparaConDibujitos.class);
+		
+		
+		this.injectProperty(Velador.class, "luz");
+		this.injectProperty(Velador.class, "marca", "Cuchuflo");
+		
+		this.injectPropertyList(Velador.class, "mamparas", MamparaConDibujitos.class, Mampara.class);		
+	}*/
+	
 	
 }
 
--- test/tadp/injector/dsl/DSLDefinitionTest.groovy
+++ test/tadp/injector/dsl/DSLDefinitionTest.groovy
@@ -2,6 +2,7 @@ package tadp.injector.dsl;
 
 import static org.junit.Assert.*
 import static tadp.injector.dsl.DSLDefinition.*
+import junit.framework.Assert;
 
 import org.junit.Test
 
@@ -15,16 +16,52 @@ import tadp.dependency.injector.fixture.velador.Velador
 class DSLDefinitionTest {
 
 	@Test
-	void "Armate un velador con Lamparita Osram"(){
+	void "armate un velador con Lamparita Osram"(){
 			
-		def bean = motorDeInyeccion {
+		def bean = motorDeInyeccion({
 			usando LamparitasOsram.class como Alumbrable.class
 			usando EnchufeTresPatas.class como Enchufable.class
 			usando Velador.class como Velador.class
 			dame Velador.class
-		}
+			})
 		
-		//motor.armateUn "Velador" usando { una "LamparitaOsram" como "Lamaprita"}
+		def velador = bean
+		
+		Assert.assertTrue(velador.getLuz() instanceof LamparitasOsram);
+		Assert.assertEquals("Cuchuflo", velador.getMarca());
+		Assert.assertNull(velador.getAlto());
+		Assert.assertEquals(2, velador.getMamparas().size());
+	}
+	
+	@Test
+	void "un relation builder tiene todas las relaciones"(){
+		def ib = new InjectionBuilder()
+		def fixture = { usando LamparitasOsram.class como Alumbrable.class
+						usando EnchufeTresPatas.class como Enchufable.class
+						usando Velador.class como Velador.class
+					   }
+		
+		ib.with fixture
+		Assert.assertEquals(3, ib.relations.size())
+		ib.relations.each { 
+			Assert.assertNotNull(it.quien)
+			Assert.assertNotNull(it.esQue)
+		}
+	}
+	
+	@Test
+	void "un relation builder sabe relacionar"(){
+		def ib = new InjectionBuilder()
+		def relBuilder = ib.usando(LamparitasOsram.class)
+		Assert.assertNotNull(relBuilder)
+		Assert.assertEquals(LamparitasOsram.class, relBuilder.quien)
+		relBuilder.como(Alumbrable.class)
+		Assert.assertEquals(Alumbrable.class, relBuilder.esQue)
+		
+		
+		Assert.assertEquals(1, ib.relations.size())
+		Assert.assertNotNull(ib.relations.get(0).quien)
+		Assert.assertNotNull(ib.relations.get(0).esQue)
 	}
 	
 }
